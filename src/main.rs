df85eda8f6b241e7530dbfee7635416cf6285ae7
yazgoo <yazgoo@gmail.com>
2021-07-07 22:20:29 +0200

    add custom actions in ron

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,7 @@ use std::io::prelude::*;
 use std::path;
 use std::process::Command;
 use std::thread;
-use umberwm::{
-    umberwm, Conf, CustomAction, EventsCallbacks, Keybind, SerializableConf, MOD_MASK_1, MOD_MASK_4,
-};
+use umberwm::{umberwm, Conf, EventsCallbacks, SerializableConf};
 
 fn save_local_conf() {
     let conf_str = include_str!("umberwm.ron");
@@ -21,12 +19,6 @@ fn save_local_conf() {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let meta = if args.len() > 1 && args[1] == "mod4" {
-        MOD_MASK_4
-    } else {
-        MOD_MASK_1
-    };
     save_local_conf();
     let serializable = SerializableConf::load().unwrap();
     umberwm(Conf {
@@ -52,71 +44,7 @@ fn main() {
             })),
         },
         /* mapping between key names (must be a name in xmodmap -pke) and user-defined actions */
-        custom_actions: vec![
-            (
-                Keybind::new(meta, "r"),
-                Box::new(|| {
-                    thread::spawn(move || {
-                        let _ = Command::new("rofi").arg("-show").arg("run").status();
-                    });
-                }) as CustomAction,
-            ),
-            (
-                Keybind::new(meta, "Return"),
-                Box::new(|| {
-                    thread::spawn(move || {
-                        let _ = Command::new("bash").arg("t").status();
-                    } /* launch my favorite terminal emulator */);
-                }),
-            ),
-            (
-                Keybind::new(meta, "s"),
-                Box::new(|| {
-                    thread::spawn(move || {
-                        let _ = Command::new("bash").arg("alsaterm").status();
-                    } /* launch alsamixer in a terminal */);
-                }),
-            ),
-            (
-                Keybind::new(meta, "l"),
-                Box::new(|| {
-                    thread::spawn(move || {
-                        let _ = Command::new("lxlock");
-                    });
-                }),
-            ),
-            (
-                Keybind::new(meta, "n"),
-                Box::new(|| {
-                    thread::spawn(move || {
-                        let _ = Command::new("xcalib").arg("-i").arg("-a").status();
-                    });
-                }),
-            ),
-            (
-                Keybind::new(meta, "x"),
-                Box::new(|| {
-                    thread::spawn(move || {
-                        let _ = Command::new("t")
-                            .arg("--class")
-                            .arg("quickmarks")
-                            .arg("--config-file")
-                            .arg("/home/yazgoo/.config/alacritty/alacritty_white.yml")
-                            .arg("-e")
-                            .arg("quickmarks")
-                            .status();
-                    });
-                }),
-            ),
-            (
-                Keybind::new(meta, "m"),
-                Box::new(|| {
-                    let _ = Command::new("autorandr").arg("--change").status();
-                }),
-            ),
-        ]
-        .into_iter()
-        .collect(),
+        custom_actions: vec![].into_iter().collect(),
     })
     .run();
 }
